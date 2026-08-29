3d53b1c6d9934ba93ec49e97569fba4b5c11368f
DefNotArham <arhamkabir231@gmail.com>
2026-08-29 16:59:38 -0400

    Create 02-valid-anagram.py

---
+++ neetcode/easy/02-valid-anagram.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        s_map = {}
+        t_map = {}
+
+        for letter in s:
+            s_map[letter] = s_map.get(letter, 0) + 1
+
+        for letter in t:
+            t_map[letter] = t_map.get(letter, 0) + 1
+
+        return s_map == t_map
